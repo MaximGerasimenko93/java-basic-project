1ff8ecc0d52bcda02db65225f2401dd949f65f39
MaximGerasimenko93 <gerasimx93@gmail.com>
2023-05-20 12:50:40 +0700

    Добавил тесты, но не все

--- src/test/java/com/gersimenko/cost/NumberConverterImplTest.java
+++ src/test/java/com/gersimenko/cost/NumberConverterImplTest.java
@@ -1,30 +1,44 @@
 package com.gersimenko.cost;
 
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
-
-
-/**
- * Тест на целое число, тест на -, тест на < 999, тест на запуск метода, тест на корректную работу метода
- */
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class NumberConverterImplTest {
-
+    @InjectMocks
+    NumberConverterImpl numberConverter;
     @Mock
-    NumberConverterImpl numberConverterImpl;
-    @Mock
-    Map<Integer, String> keyNumbersMap;
+    Map<Integer, String> keyNumbersMapMock;
 
     @Test
-    @DisplayName("Тест ввода отрицательного числа")
-    public void negativeKeyTest() {
+    public void testToConvert() {
+        when(keyNumbersMapMock.get(1)).thenReturn("один");
+        when(keyNumbersMapMock.get(2)).thenReturn("два");
+        when(keyNumbersMapMock.get(3)).thenReturn("три");
+        when(keyNumbersMapMock.get(4)).thenReturn("четыре");
+        when(keyNumbersMapMock.get(5)).thenReturn("пять");
+        when(keyNumbersMapMock.get(6)).thenReturn("шесть");
+        when(keyNumbersMapMock.get(7)).thenReturn("семь");
+        when(keyNumbersMapMock.get(8)).thenReturn("восемь");
+        when(keyNumbersMapMock.get(9)).thenReturn("девять");
+        when(keyNumbersMapMock.get(10)).thenReturn("десять");
 
+        assertEquals("один", numberConverter.toConvert(1));
+        assertEquals("два", numberConverter.toConvert(2));
+        assertEquals("три", numberConverter.toConvert(3));
+        assertEquals("четыре", numberConverter.toConvert(4));
+        assertEquals("пять", numberConverter.toConvert(5));
+        assertEquals("шесть", numberConverter.toConvert(6));
+        assertEquals("семь", numberConverter.toConvert(7));
+        assertEquals("восемь", numberConverter.toConvert(8));
+        assertEquals("девять", numberConverter.toConvert(9));
+        assertEquals("десять", numberConverter.toConvert(10));
     }
-
 }
--- src/test/java/com/gersimenko/cost/NumberReaderImplTest.java
+++ src/test/java/com/gersimenko/cost/NumberReaderImplTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -15,29 +17,38 @@ class NumberReaderImplTest {
 
     @InjectMocks
     NumberReaderImpl numberReaderImpl;
+    Scanner scanner = mock(Scanner.class);
 
     @Test
     @DisplayName("Тест ввода отрицательного числа")
     void readingNegativeNumberTest() {
         int result = numberReaderImpl.readNumber();
-        assertTrue(result < 0, "Вы ввели отрицательное число");
+        when(result).thenReturn(-5);
+        assertTrue(result > 0, "Вы ввели отрицательное число");
     }
 
     @Test
     @DisplayName("Тест ввода числа больше 1000")
     void readingThousandNumberTest() {
         int result = numberReaderImpl.readNumber();
-        assertTrue(result > 999, "Вы ввели число больше 999");
+        when(result).thenReturn(1000);
+        assertTrue(result < 999, "Вы ввели число больше 999");
+    }
+    @Test
+    @DisplayName("Тест на null")
+    void readingNullResultTest() {
+        int result = numberReaderImpl.readNumber();
+        assertNotNull(result);
     }
 
     @Test
     @DisplayName("Тест нуля")
     void readingZeroTest() {
         int result = numberReaderImpl.readNumber();
-        assertTrue(result == 0, "0 рублей не может быть");
+        when(result).thenReturn(0);
+        assertTrue(result != 0, "0 рублей не может быть");
     }
 
-    // подумать как протестировать ввод целого значения вместо чего-либо другого
     @Test
     @DisplayName("Тест ввода числа")
     void readingIntegerNumberTest() {
